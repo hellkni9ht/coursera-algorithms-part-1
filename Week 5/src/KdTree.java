be5a245d05472b36e3e7791afb6351e9c6053e26
baal <oberlin3@mackiev.com>
2015-11-14 19:39:44 +0200

    Implement kd-tree.

--- Week 5/src/KdTree.java
+++ Week 5/src/KdTree.java
@@ -1,5 +1,10 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdDraw;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by Bazna on 11/7/2015.
@@ -22,15 +27,36 @@ public class KdTree {
         }
 
         public int compare(final Point2D pointToCompare) {
-            if (level % 2 == 0) {
+            if (isXLevel()) {
                 return Double.compare(pointToCompare.x(), point.x());
             } else {
                 return Double.compare(pointToCompare.y(), point.y());
             }
         }
+
+        public boolean isXLevel() {
+            return level % 2 == 0;
+        }
+
+        public boolean intersectsWithSplitingLine(RectHV rect) {
+            if (isXLevel()) {
+                return point.x() >= rect.xmin() && point.x() <= rect.xmax();
+            }
+            else {
+                return point.y() >= rect.ymin() && point.y() <= rect.ymax();
+            }
+        }
+
+        public boolean isRightTop(RectHV rect) {
+            if (isXLevel()) {
+                return rect.xmin() < point.x() && point.x() < rect.xmax();
+            } else {
+                return rect.ymin() < point.y() && point.y() < rect.ymax();
+            }
+        }
     }
 
-    private Node root;
+    private Node rootNode;
 
     // construct an empty set of points
     public KdTree() {
@@ -43,15 +69,11 @@ public class KdTree {
     }
     // number of points in the set
     public int size() {
-        return size(root);
+        return size(rootNode);
     }
 
     public int size(Node node) {
         return node == null ? 0 : node.size;
-    }
-
-    private boolean isXLevel(int level) {
-        return level % 2 == 0;
     }
 
     private void ensureInput(Object object) {
@@ -69,14 +91,14 @@ public class KdTree {
         RectHV lbRect = null;
         RectHV rtRect = null;
         if (node.lb == null && compareResult <= 0) {
-            if (isXLevel(level)) {
+            if (node.isXLevel()) {
                 lbRect = new RectHV(node.rect.xmin(), node.point.x(), node.rect.ymin(), node.rect.ymax());
             } else {
                 lbRect = new RectHV(node.rect.xmin(), node.rect.xmax(), node.rect.ymin(), node.point.y());
             }
         }
         else if (compareResult >= 0) {
-            if (isXLevel(level)) {
+            if (node.isXLevel()) {
                 lbRect = new RectHV(node.point.x(), node.rect.xmax(), node.rect.ymin(), node.rect.ymax());
             } else {
                 lbRect = new RectHV(node.rect.xmin(), node.rect.xmax(), node.point.y(), node.rect.ymax());
@@ -98,7 +120,7 @@ public class KdTree {
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
         ensureInput(p);
-        root = put(root, p, 0, new RectHV(0, 0, 1, 1));
+        rootNode = put(rootNode, p, 0, new RectHV(0, 0, 1, 1));
     }
 
     private Point2D get(final Node node, final Point2D searchedPoint, final int level) {
@@ -117,23 +139,111 @@ public class KdTree {
     // does the set contain point p?
     public boolean contains(Point2D p) {
         ensureInput(p);
-        return get(root, p, 0) != null;
+        return get(rootNode, p, 0) != null;
     }
 
     // draw all points to standard draw
     public void draw() {
+        draw(rootNode);
+    }
 
+    private void draw(Node node) {
+        if (node == null)
+            return;
+
+        // draw the node
+        StdDraw.point(node.point.x(), node.point.y());
+
+        // draw the left/bottom subtree
+        draw(node.lb);
+
+        // draw the right/top subtree
+        draw(node.rt);
     }
 
     // all points that are inside the rectangle
     public Iterable<Point2D> range(RectHV rect) {
         ensureInput(rect);
+
+        List<Point2D> foundPoints = Collections.emptyList();
+        range(rect, rootNode, foundPoints);
+        return foundPoints;
+    }
+
+    private void range(RectHV queryRect, Node node, List<Point2D> foundPoints) {
+        if (node == null)
+            return;
+
+        boolean hasIntersection = node.intersectsWithSplitingLine(queryRect);
+        if (hasIntersection) {
+            if (queryRect.contains(node.point))
+                foundPoints.add(node.point);
+
+            range(queryRect, node.lb, foundPoints);
+            range(queryRect, node.rt, foundPoints);
+        } else if (node.isRightTop(queryRect)) {
+            range(queryRect, node.rt, foundPoints);
+        } else {
+            range(queryRect, node.lb, foundPoints);
+        }
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
         ensureInput(p);
 
+        if (rootNode == null)
+            return null;
+
+        return nearest(p, rootNode, rootNode.point);
+    }
+
+    private Point2D nearest(Point2D queryPoint, Node node, Point2D currentClosestPoint) {
+        if (node == null)
+            return null;
+
+        Point2D closestPoint = currentClosestPoint;
+
+        if (node.point.distanceSquaredTo(queryPoint) < currentClosestPoint.distanceSquaredTo(queryPoint)) {
+            closestPoint = node.point;
+        }
+
+        // choose the subtree that is on the same side of the splitting line as the query point as the first subtree to explore
+        Point2D candidate = null;
+        int compare = node.compare(queryPoint);
+        if (compare < 0) {
+            candidate = nearest(queryPoint, node, closestPoint);
+        }
+        else {
+            candidate = nearest(queryPoint, node, closestPoint);
+        }
+
+        if (candidate != null && candidate.distanceSquaredTo(queryPoint) < closestPoint.distanceSquaredTo(queryPoint))
+            closestPoint = candidate;
+
+        // Check if it possible to prune the second subtree.
+        // Pruning rule: if the closest point discovered so far is closer than the distance between
+        // the query point and the rectangle corresponding to a node,
+        // there is no need to explore that node (or its subtrees).
+        if (compare < 0) {
+            if (node.rt != null) {
+                if (node.rt.rect.distanceSquaredTo(queryPoint) < candidate.distanceSquaredTo(queryPoint)) {
+                    candidate = nearest(queryPoint, node.rt, closestPoint);
+                }
+            }
+        }
+        else {
+            if (node.lb != null) {
+                if (node.lb.rect.distanceSquaredTo(queryPoint) < candidate.distanceSquaredTo(queryPoint)) {
+                    candidate = nearest(queryPoint, node.lb, closestPoint);
+                }
+            }
+        }
+
+        if (candidate != null && candidate.distanceSquaredTo(queryPoint) < closestPoint.distanceSquaredTo(queryPoint))
+            closestPoint = candidate;
+
+        return closestPoint;
     }
 
     // unit testing of the methods (optional)
